864d8896b180e307269e347324540631c145c38d
Colin Monaghan <cleemonaghan@gmail.com>
2021-11-13 13:29:08 -0800

    Added event listeners for each button

--- simone.js
+++ simone.js
@@ -1,4 +1,30 @@
+let startButton = document.querySelector("#play");
+startButton.addEventListener("click", function (evt) {
+	console.log("play clicked");
+
+	try {
+		//fetch the greeting sequence from the API
+		//retrieve the number of rounds to play
+		//fetch a solution sequence from the API
+	} catch (error) {
+		//if something went wrong, print the error to the console
+		console.log(error);
+	}
+});
+
 let redButton = document.querySelector("#redSq");
-redButton.addEventListener("onclick", function (evt) {
+redButton.addEventListener("click", function (evt) {
 	console.log("red clicked");
+});
+let blueButton = document.querySelector("#blueSq");
+blueButton.addEventListener("click", function (evt) {
+	console.log("blue clicked");
+});
+let greenButton = document.querySelector("#greenSq");
+greenButton.addEventListener("click", function (evt) {
+	console.log("green clicked");
+});
+let yellowButton = document.querySelector("#yellowSq");
+yellowButton.addEventListener("click", function (evt) {
+	console.log("yellow clicked");
 });
