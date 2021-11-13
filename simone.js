1f24820c518eceb3b8d9eeb340a92c3adcd9dd6f
Colin Monaghan <cleemonaghan@gmail.com>
2021-11-13 13:59:34 -0800

    Outlined playGame() function

--- simone.js
+++ simone.js
@@ -2,8 +2,6 @@ const { default: axios } = require("axios");
 
 let startButton = document.querySelector("#play");
 startButton.addEventListener("click", async function (evt) {
-	console.log("play clicked");
-
 	try {
 		//fetch the greeting sequence from the API
 		let greeting = await axios.get(
@@ -44,4 +42,33 @@ yellowButton.addEventListener("click", function (evt) {
 	console.log("yellow clicked");
 });
 
-function playGame(greeting, solution, rounds) {}
+function playGame(greeting, solution, rounds) {
+	// display greeting sequence
+	// 120 ms interval between each button
+
+	// 4 second delay
+	// start of the first round
+	for (let seqLenth = 0; seqLenth < rounds; seqLenth++) {
+		for (let i = 0; i < seqLenth; i++) {
+			//display sequence of seqLenth
+			// 400 ms interval between each button
+			if (incorrect) {
+				//play wrong.wav followed directly with lose.wav
+				// change background to bright pink
+				// display "Incorrect! You lose!"
+				//return
+			}
+
+			//if button press is correct, display "So far so good! x more to go!"
+		}
+
+		//at end of round, display "Good job! Prepare for next round."
+		// 800 ms delay
+		// display "Round x of seqLenth"
+		// 800 ms delay.
+	}
+
+	//If the player wins, play the appropriate sound bites
+	//change the background to DeepSkyBlue
+	//display "Yay you win!"
+}
