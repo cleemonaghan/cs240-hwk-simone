b5c1110daeeeb364572907b6313cec1693b13fa6
Colin Monaghan <cleemonaghan@gmail.com>
2021-11-13 13:44:15 -0800

    Updated contents of the eventListener function for the startButton

--- simone.js
+++ simone.js
@@ -1,11 +1,26 @@
+const { default: axios } = require("axios");
+
 let startButton = document.querySelector("#play");
-startButton.addEventListener("click", function (evt) {
+startButton.addEventListener("click", async function (evt) {
 	console.log("play clicked");
 
 	try {
 		//fetch the greeting sequence from the API
+		let greeting = await axios.get(
+			"http://cs.pugetsound.edu/~dchiu/cs240/api/simone/",
+			{ cmd: "start" }
+		);
+
 		//retrieve the number of rounds to play
+		let rounds = document.querySelector("#rounds").value;
+
 		//fetch a solution sequence from the API
+		let solution = await axios.get(
+			"http://cs.pugetsound.edu/~dchiu/cs240/api/simone/",
+			{ cmd: "getSolution", rounds: rounds }
+		);
+
+		playGame(greeting, solution, rounds);
 	} catch (error) {
 		//if something went wrong, print the error to the console
 		console.log(error);
@@ -28,3 +43,5 @@ let yellowButton = document.querySelector("#yellowSq");
 yellowButton.addEventListener("click", function (evt) {
 	console.log("yellow clicked");
 });
+
+function playGame(greeting, solution, rounds) {}
